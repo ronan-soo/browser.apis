6b931195440a9b52d25d946b4b19331e8022d7c6
redoxide-dev <88243488+redoxide-dev@users.noreply.github.com>
2021-09-26 02:03:54 +0800

    update

--- Redoxide.Browser.APIs/src/layouts/Layout.js
+++ Redoxide.Browser.APIs/src/layouts/Layout.js
@@ -1,38 +1,21 @@
 import * as React from "react";
-import { styled, useTheme } from "@mui/material/styles";
-import Box from "@mui/material/Box";
-import Drawer from "@mui/material/Drawer";
-import CssBaseline from "@mui/material/CssBaseline";
-import AppBar from "@mui/material/AppBar";
-import Toolbar from "@mui/material/Toolbar";
-import List from "@mui/material/List";
-import Typography from "@mui/material/Typography";
-import Divider from "@mui/material/Divider";
-import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import { useTheme } from "@mui/material/styles";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Drawer from "@mui/material/Drawer";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
 
 const drawerWidth = 240;
-
-// const NavBar = styled(AppBar, {
-//     shouldForwardProp: (prop) => prop !== "open"
-// })(({ theme, open }) => ({
-//     transition: theme.transitions.create(["margin", "width"], {
-//         easing: theme.transitions.easing.sharp,
-//         duration: theme.transitions.duration.leavingScreen
-//     }),
-//     ...(open && {
-//         width: `calc(100% - ${drawerWidth}px)`,
-//         marginLeft: `${drawerWidth}px`,
-//         transition: theme.transitions.create(["margin", "width"], {
-//             easing: theme.transitions.easing.easeOut,
-//             duration: theme.transitions.duration.enteringScreen
-//         })
-//     })
-// }));
 
 export default function PersistentDrawerLeft() {
     const theme = useTheme();
@@ -109,9 +92,7 @@ export default function PersistentDrawerLeft() {
                 <Toolbar>
                     <IconButton
                         color="inherit"
-                        aria-label="open drawer"
                         onClick={toggleDrawer}
-                        edge="start"
                         sx={{ mr: 2 }}
                     >
                         <MenuIcon />
@@ -134,8 +115,7 @@ export default function PersistentDrawerLeft() {
                 anchor="left"
                 open={open}
             >
-                <DrawerHeader>
-                </DrawerHeader>
+                <DrawerHeader />
                 <Divider />
                 <List>
                     {["All mail", "Trash", "Spam"].map((text, index) => (
